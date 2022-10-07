63f7bf5676cd4c0b2e8136e40cc8600fa5740ea5
nathan-kedinger <51140084+nathan-kedinger@users.noreply.github.com>
2022-10-07 15:50:46 +0200

    correction du bug dans services.yaml

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,7 +2,6 @@
 
 namespace App\Controller;
 
-use App\Class\MailClass;
 use App\Classes\EventsClass;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
